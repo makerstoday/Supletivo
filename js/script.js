b6352bcb69bce54aceb285f39cf0adf381cf9bd7
makerstoday <marcelobarbosafb@gmail.com>
2023-01-31 23:28:55 -0300

    validando-formulario
    javascript e estrutura html

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
-const Form = document.querySelector("#form");
+const form = document.querySelector("#form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const phoneInput = document.querySelector("#phone");
 const interestSelect = document.querySelector("#interest");
 
-form.addEventListener("submit",(event) => {
-    event.preventDeFault();
 
-    if (emailInput.value  ("")) {
-        alert("por favor");
+    //função de validação
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //verificar se o nome é valido
+    if (nameInput.value === ("")){
+        alert("Por favor escreva um nome valido!!");
         return;
     }
+
+    // verificar se o email é valido
+    if (emailInput.value === ("")){
+        alert("Por favor escreva um email valido!!");
+        return;
+    }
+
+     // verificar se o phone é valido
+     if (phoneInput.value === ("")){
+    alert("Por favor escreva um email valido!!");
+    return;
+    }   
+
+
+    // selecione huma opção valida
+    if (interestSelect.value === "" ) {
+        alert("Por favor selecione uma opção!!");
+        return;
+    }
+    //se todos os campos tiverem preenchido , enviar
     form.submit();
-})
+    
+});
+
+/*const button = document.getElementById('btn');
+
+button.addEventListener('click', function(event) {
+  validateForm();
+  sendForm();
+});*/
